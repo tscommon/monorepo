a45242991f82898b93417b4c7ccf78b8ce1dea34
igrek8 <7078731+igrek8@users.noreply.github.com>
2025-09-14 13:34:21 +0200

    refine mutex

--- packages/mutex/src/Mutex.ts
+++ packages/mutex/src/Mutex.ts
@@ -1,22 +1,23 @@
 import { MutexData } from './MutexData';
-import { MutextGuard } from './MutexGuard';
+import { MutexGuard } from './MutexGuard';
 import { MutexState } from './MutexState';
 import { MutexTryGuard } from './MutexTryGuard';
 
 export class Mutex<T> {
-  private readonly state = new MutexState();
-  private readonly data: MutexData<T>;
+  readonly #state: MutexState;
+  readonly #data: MutexData<T>;
 
   public constructor(data: T) {
-    this.data = new MutexData<T>(data);
+    this.#state = new MutexState();
+    this.#data = new MutexData<T>(data);
     Object.freeze(this);
   }
 
-  public lock(): MutextGuard<MutexData<T>> {
-    return new MutextGuard<MutexData<T>>(this.state, this.data);
+  public lock(): MutexGuard<MutexData<T>> {
+    return new MutexGuard<MutexData<T>>(this.#state, this.#data);
   }
 
-  public tryLock(): MutextGuard<MutexData<T> | undefined> {
-    return new MutexTryGuard<MutexData<T> | undefined>(this.state, this.data);
+  public tryLock(): MutexGuard<MutexData<T> | undefined> {
+    return new MutexTryGuard<MutexData<T> | undefined>(this.#state, this.#data);
   }
 }
--- packages/mutex/src/MutexState.ts
+++ packages/mutex/src/MutexState.ts
@@ -1,8 +1,26 @@
 export class MutexState {
-  public constructor(
-    public owner?: object,
-    public queue?: Promise<unknown>,
-  ) {
-    Object.seal(this);
+  #owner?: object;
+  #queue?: Promise<unknown>;
+
+  public get owner(): object | undefined {
+    return this.#owner;
+  }
+
+  public set owner(value: object | undefined) {
+    this.#owner = value;
+  }
+
+  public get queue(): Promise<unknown> | undefined {
+    return this.#queue;
+  }
+
+  public set queue(value: Promise<unknown> | undefined) {
+    this.#queue = value;
+  }
+
+  public constructor(owner?: object, queue?: Promise<unknown>) {
+    this.#owner = owner;
+    this.#queue = queue;
+    Object.freeze(this);
   }
 }
